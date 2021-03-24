7a49ffe75d100dbda3c5fd4a440b6d11c0153fe8
ureeze <ureeze07@gmail.com>
2021-03-25 02:49:13 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootgradledemo/SpringbootgradledemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgradledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgradledemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootgradledemo/web/HelloResponseDtoTest.java
@@ -0,0 +1,4 @@
+package com.example.springbootgradledemo.web;
+
+public class HelloResponseDtoTest {
+}
